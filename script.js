666ff1e3de532ed3f8c25b14074d16e9631a52e9
stefaniewangphd <stefaniegwang@gmail.com>
2023-01-11 13:02:07 -0600

    Modify page layout, update grid size based on slider input

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const gridBox=document.querySelector('#gridBox');
 const grid=document.querySelector('.box');
-let gridSize=prompt("choose the grid size");
+const slider = document.getElementById("myRange");
+const output = document.getElementById("sliderOutput");
+const resize = document.getElementById("resize");
+output.innerHTML = slider.value;
 
+let gridSize=output.innerHTML;
 grid.setAttribute("style", `grid-template-columns: repeat(${gridSize},2fr)`)
-// grid.setAttribute("style", `grid-template-rows: repeat(${gridSize},2fr)`)
-
 
 for (i=0; i<gridSize*gridSize;i++) {
     const cell=document.createElement('div');
@@ -15,7 +17,23 @@ for (i=0; i<gridSize*gridSize;i++) {
     gridBox.append(cell);
 }
 
-const eraseBtn = document.querySelector('#reset');
-const resetBtn = document.querySelector('#reset');
+let changeGridSize= function () {
+    output.innerHTML = slider.value;
+    gridBox.innerHTML='';
+    gridSize=slider.value;
+    grid.setAttribute("style", `grid-template-columns: repeat(${gridSize},2fr)`)
+    for (i=0; i<gridSize*gridSize;i++) {
+        const cell=document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener("mouseover",function (){
+            cell.setAttribute("style", "background-color: pink;")
+        });
+        gridBox.append(cell);
+    }
+}
 
+slider.addEventListener('input', changeGridSize, false);
+
+const resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener('click',() => location.reload());
+
