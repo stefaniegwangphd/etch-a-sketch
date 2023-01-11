a687cd716e8750f10bb0f81998f1b6a1954caa64
stefaniewangphd <stefaniegwang@gmail.com>
2023-01-11 15:05:59 -0600

    Create default/rainbow/erase buttons, add script to execute colors selected

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const resize = document.getElementById("resize");
 output.innerHTML = slider.value;
 
 let gridSize=output.innerHTML;
-grid.setAttribute("style", `grid-template-columns: repeat(${gridSize},2fr)`)
+grid.setAttribute("style", `grid-template-columns: repeat(${gridSize},2fr)`);
 
 for (i=0; i<gridSize*gridSize;i++) {
     const cell=document.createElement('div');
     cell.classList.add('cell');
-    cell.addEventListener("mouseover",function (){
-        cell.setAttribute("style", "background-color: pink;")
+    cell.addEventListener("mouseover",function(event){
+        event.target.style.backgroundColor = 'pink';
     });
     gridBox.append(cell);
 }
@@ -25,8 +25,8 @@ let changeGridSize= function () {
     for (i=0; i<gridSize*gridSize;i++) {
         const cell=document.createElement('div');
         cell.classList.add('cell');
-        cell.addEventListener("mouseover",function (){
-            cell.setAttribute("style", "background-color: pink;")
+        cell.addEventListener("mouseover",function(event){
+            event.target.style.backgroundColor = 'pink';
         });
         gridBox.append(cell);
     }
@@ -34,6 +34,43 @@ let changeGridSize= function () {
 
 slider.addEventListener('input', changeGridSize, false);
 
+const defaultBtn = document.querySelector('#default');
+defaultBtn.addEventListener("click", function(){
+    gridSize=slider.value;
+    let cell=grid.children;
+    for (i=0; i<gridSize*gridSize; i++) {
+        cell[i].addEventListener('mouseover',function(event){
+            event.target.style.backgroundColor = 'pink';
+        });
+    }
+});
+
+function getRandomColor() {
+    let randomColor='#'+Math.floor(Math.random()*16777215).toString(16);
+    return randomColor
+}
+
+const rainbowBtn = document.querySelector('#rainbow');
+rainbowBtn.addEventListener("click", function(){
+    gridSize=slider.value;
+    let cell=grid.children;
+    for (i=0; i<gridSize*gridSize; i++) {
+        cell[i].addEventListener('mouseover',function(event){
+            event.target.style.backgroundColor = getRandomColor();
+        });
+    }
+});
+
+const eraseBtn = document.querySelector('#erase');
+eraseBtn.addEventListener("click", function(){
+    gridSize=slider.value;
+    let cell=grid.children;
+    for (i=0; i<gridSize*gridSize; i++) {
+        cell[i].addEventListener('mouseover',function(event){
+            event.target.style.backgroundColor = 'white';
+        });
+    }
+});
+
 const resetBtn = document.querySelector('#reset');
 resetBtn.addEventListener('click',() => location.reload());
-
