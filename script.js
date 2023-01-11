61785e8367412db18b7f4d92a95b3c5ca51d914c
stefaniewangphd <stefaniegwang@gmail.com>
2023-01-11 10:49:24 -0600

    Update grid size with user input

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const gridBox=document.querySelector('#gridBox');
-let gridSize=16;
+const grid=document.querySelector('.box');
+let gridSize=prompt("choose the grid size");
+
+grid.setAttribute("style", `grid-template-columns: repeat(${gridSize},2fr)`)
+// grid.setAttribute("style", `grid-template-rows: repeat(${gridSize},2fr)`)
+
 
 for (i=0; i<gridSize*gridSize;i++) {
     const cell=document.createElement('div');
@@ -10,6 +15,7 @@ for (i=0; i<gridSize*gridSize;i++) {
     gridBox.append(cell);
 }
 
+const eraseBtn = document.querySelector('#reset');
 const resetBtn = document.querySelector('#reset');
 
 resetBtn.addEventListener('click',() => location.reload());
